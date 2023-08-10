96fe4dc98807ae3efa29770b2cd43f8334dc8d19
Raheem ullah <141299850+raheemullah30@users.noreply.github.com>
2023-08-10 21:26:38 +0500

    Add files via upload

---
+++ class and object.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class raheem
+{
+
+private:
+	int a,b,c;
+	public:
+		
+		void getdata();
+	{
+	
+	cout<<"enter the number of a , b,c";
+	cin>>a>>b>>c;
+}
+{
+void putdata();
+cout<<"addition"<<a+b+c;
+};
+ int main()
+
+raheem r;
+r.getdata();
+
+return 0;
+ 
